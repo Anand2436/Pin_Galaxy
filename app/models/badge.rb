9c6ec7454d01219ce625ef09bcf2f5ad0a966089
Anand <anandrao0058@gmail.com>
2017-07-25 21:37:31 +0530

    Trying to figure out image error 2

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,7 +2,7 @@ class Badge < ApplicationRecord
 	acts_as_votable
 	belongs_to :user
 
-	has_attached_file :image, styles: { medium: "320x300>", thumb: "200x200>" }, default_url: "/images/:style/missing.jpg"
+	has_attached_file :image, styles: { medium: "320x300>"}, default_url: "/images/:style/missing.jpg"
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
 end
